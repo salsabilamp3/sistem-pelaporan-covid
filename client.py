da25d1ef550fdf2ce5b9ad3666c825a4d0d740e7
salsabilamp3 <salsabilamp3@gmail.com>
2024-06-05 12:52:03 +0700

    add code to set penjemput

--- client.py
+++ client.py
@@ -1,8 +1,11 @@
 import json
+import uuid
 from google.cloud import pubsub_v1
 import os
 from dotenv import load_dotenv
+from concurrent.futures import TimeoutError
 
+# Memuat variabel lingkungan dari file .env
 load_dotenv()
 
 credentials_path = os.getenv('CREDENTIALS_PATH')
@@ -23,7 +26,8 @@ def send_message(data):
 def callback(message):
     try:
         response = message.data.decode('utf-8')
-        print("Received response:", response)
+        id_laporan, respon_detail = response.split(';', 1)
+        print(f"Received response for IDLaporan {id_laporan}: {respon_detail}")
         message.ack()
     except Exception as e:
         print(f"Terjadi kesalahan saat menangani respons dari server: {e}")
@@ -39,23 +43,40 @@ def create_subscriber():
         print(f"Terjadi kesalahan saat membuat subscriber: {e}")
         return None, None
 
-# Data contoh laporan
+data_laporan_path = r'data\data_laporan.json'
+
 laporan = {}
 field_width = 30
 
 try:
+    laporan['IDLaporan'] = str(uuid.uuid4())
     laporan['NIK'] = input(f"{'Masukkan NIK':<{field_width}} : ")
     laporan['Nama Pelapor'] = input(f"{'Masukkan Nama Pelapor':<{field_width}} : ")
     laporan['Nama Terduga Covid'] = input(f"{'Masukkan Nama Terduga Covid':<{field_width}} : ")
     laporan['Alamat Terduga Covid'] = input(f"{'Masukkan Alamat Terduga Covid':<{field_width}} : ")
     laporan['Gejala'] = input(f"{'Masukkan Gejala':<{field_width}} : ")
+    laporan['Penjemputan'] = {
+            "ID_Penjemput": "",
+            "Nama_Lengkap": "",
+            "Nomor_Telepon": "",
+            "Nomor_Kendaraan": "",
+            "Waktu_Penjemputan": ""
+    }  
+
+    if os.path.exists(data_laporan_path):
+        with open(data_laporan_path, 'r') as json_file:
+            data_laporan = json.load(json_file)
+    else:
+        data_laporan = []
+    
+    data_laporan.append(laporan)
 
     # Tulis data laporan ke file JSON
-    with open(r'data\data_laporan.json', 'w') as json_file:
-        json.dump(laporan, json_file, indent=4)
+    with open(data_laporan_path, 'w') as json_file:
+        json.dump(data_laporan, json_file, indent=4)
 
     # Kirim laporan ke server
-    send_message(';'.join([laporan[key] for key in laporan]))
+    send_message(';'.join([laporan[key] for key in list(laporan.keys())[:-1]]))
 
     print("Laporan dikirim")
 
--- server.py
+++ server.py
@@ -14,37 +14,90 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
 with open(r"data\data_kependudukan.json") as f:
     data_kependudukan = json.load(f)
 
+# Baca data penjemput dari file JSON
+with open(r"data\data_laporan.json") as f:
+    data_laporan = json.load(f)
+
+# Baca data penjemput dari file JSON
+with open(r"data\data_informasi_penjemput.json") as f:
+    data_penjemput = json.load(f)
+
 # Fungsi untuk menangani pesan yang diterima dari topik
 def callback(message):
     # Lakukan validasi data pesan (contoh sederhana)
     data = message.data.decode('utf-8').split(';')
     print(data)
-    if len(data) != 5:
+    if len(data) != 6:
         print("Invalid message format")
         message.ack()
         return
 
-    # Dapatkan NIK dan nama dari data pesan
-    nik = data[0]
-    nama = data[1]
+    # Dapatkan IDLaporan, NIK, dan nama dari data pesan
+    id_laporan = data[0]
+    nik = data[1]
+    nama = data[2]
 
     # Lakukan validasi NIK dan nama dengan data kependudukan dari file JSON
     valid_nik, valid_nama, reason = validate_nik(nik, nama)
     if valid_nik and valid_nama:
-        # Respon kepada client dengan informasi waktu dan jumlah orang penjemputan
-        waktu = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        # Pilih penjemput dengan last_schedule terkecil
+        selected_penjemput = min(data_penjemput, key=lambda x: x.get('last_schedule', float('inf')))
+        
+        # Respon kepada client dengan informasi waktu penjemputan, nama penjemput, dan jumlah orang penjemputan
+        waktu_penjemputan = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        nama_penjemput = selected_penjemput["Nama_Lengkap"]
         jumlah_orang = 1  # Misalnya hanya 1 orang penjemputan
-        respon = f"{waktu}, {nama}, Jumlah Orang Penjemputan: {jumlah_orang}"
-    else:
-        respon = f"Kesalahan: {reason}"
 
+        # Perbarui last_schedule penjemput yang dipilih
+        last_schedule = selected_penjemput.get("last_schedule")
+        if last_schedule and last_schedule >= waktu_penjemputan:
+            # Jika last_schedule belum terlewati, atur waktu penjemputan 10 menit setelah last_schedule
+            new_schedule = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.mktime(time.strptime(last_schedule, "%Y-%m-%d %H:%M:%S")) + 600))
+            selected_penjemput["last_schedule"] = new_schedule
+        else:
+            # Jika last_schedule sudah terlewati atau belum diatur, atur waktu penjemputan sama dengan waktu sekarang
+            selected_penjemput["last_schedule"] = waktu_penjemputan
+
+        # Tambahkan object penjemputan ke dalam data laporan
+        for penjemput in data_penjemput:
+            if penjemput["ID_Penjemput"] == selected_penjemput["ID_Penjemput"]:
+                penjemput["last_schedule"] = selected_penjemput["last_schedule"]
+                break
+        
+        # Tulis data laporan ke file JSON
+        with open("data\data_informasi_penjemput.json", 'w') as json_file:
+            json.dump(data_penjemput, json_file, indent=4)
+
+        waktu_penjemputan = selected_penjemput["last_schedule"]
+        
+        # Tulis data laporan yang sudah diperbarui ke file JSON
+        with open("data\data_laporan.json", 'r+') as json_file:
+            data_laporan = json.load(json_file)
+            for laporan in data_laporan:
+                if laporan["IDLaporan"] == id_laporan:
+                    laporan["Penjemputan"] = {
+                        "ID_Penjemput": selected_penjemput["ID_Penjemput"],
+                        "Nama_Lengkap": selected_penjemput["Nama_Lengkap"],
+                        "Nomor_Telepon": selected_penjemput["Nomor_Telepon"],
+                        "Nomor_Kendaraan": selected_penjemput["Nomor_Kendaraan"],
+                        "Waktu_Penjemputan": waktu_penjemputan
+                    }
+                    break
+            json_file.seek(0)  # Pindah ke awal file
+            json.dump(data_laporan, json_file, indent=4)
+            json_file.truncate()  # Potong ke panjang yang benar jika perlu
+        
+        respon_detail = f"Waktu Penjemputan: {waktu_penjemputan}, Nama Penjemput: {nama_penjemput}, Jumlah Orang Penjemputan: {jumlah_orang}"
+        
+    else:
+        respon_detail = f"Kesalahan: {reason}"
+
+    respon = f"{id_laporan};{respon_detail}"
     print(f"Respon: {respon}")
 
     # Kirim respons ke client menggunakan message queue
     send_response(respon)
     message.ack()
-
-
 # Fungsi untuk validasi NIK dan nama berdasarkan data kependudukan
 def validate_nik(nik, nama):
     valid_nik = False
@@ -68,7 +121,7 @@ def validate_nik(nik, nama):
             reason = f"NIK {nik} tidak ditemukan"
     return valid_nik, valid_nama, reason
 
-# Fungsi untuk mengirim respons ke client menggunakan mess~age queue
+# Fungsi untuk mengirim respons ke client menggunakan message queue
 def send_response(respon):
     publisher = pubsub_v1.PublisherClient()
     topic_path = "projects/sistem-siaga-covid/topics/response"
